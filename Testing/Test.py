e475beb32734c355d165a3abc8d14af12b13a0fc
Mayur Sharma <matrix4reloaded2000@gmail.com>
2021-08-24 18:39:39 +0530

    What

---
+++ Testing/Test.py
@@ -0,0 +1,7 @@
+from tkinter import *
+win = Tk()
+win.geometry("500x500")
+win.title("Hello World")
+Label(win, text='Works', font = (None,20)).pack()
+Label(win, text='New Window', font = (None,20)).pack()
+win.mainloop()
