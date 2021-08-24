7c6df344003122adc298ce452b594dd1938b2779
Mayur Sharma <matrix4reloaded2000@gmail.com>
2021-08-24 18:37:05 +0530

    New Folder

--- Test.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-win = Tk()
-win.geometry("500x500")
-win.title("Hello World")
-Label(win, text='Works', font = (None,20)).pack()
-Label(win, text='New Window', font = (None,20)).pack()
-win.mainloop()
