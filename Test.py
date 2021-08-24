d8e4c81db65d5db8da0677583850db9e4d11daa2
Mayur Sharma <matrix4reloaded2000@gmail.com>
2021-08-24 18:23:21 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
-import tkinter
+from tkinter import *
 win = Tk()
 win.geometry("500x500")
 win.title("Hello World")
 Label(win, text='Works', font = (None,20)).pack()
+Label(win, text='New Window', font = (None,20)).pack()
 win.mainloop()
