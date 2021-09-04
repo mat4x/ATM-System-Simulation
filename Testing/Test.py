a43dffe6d6ef154ef819332e35adbd2cfbe05d0a
Mayur Sharma <matrix4reloaded2000@gmail.com>
2021-09-04 10:23:39 +0530

    Update Test.py

--- Testing/Test.py
+++ Testing/Test.py
@@ -4,4 +4,22 @@ win.geometry("500x500")
 win.title("Hello World")
 Label(win, text='Works', font = (None,20)).pack()
 Label(win, text='New Window', font = (None,20)).pack()
+
+L = ["arrow", "circle", "clock", "cross", "dotbox", "exchange", "fleur", "heart", "heart", "man",
+"mouse",
+"pirate",
+"plus",
+"shuttle",
+"sizing",
+"spider",
+"spraycan",
+"star",
+"target",
+"tcross",
+"trek",
+"watch"]
+
+for i in L:
+    Button(win, text=i, cursor=i).pack(fill=X)
+
 win.mainloop()
