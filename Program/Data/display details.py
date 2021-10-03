2b8d967c92432e15bece4b20be3109ed2b90ac81
nanya16 <ananya.mishra467@nmims.edu.in>
2021-10-03 15:45:06 +0530

    added display details

---
+++ Program/Data/display details.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import config
+from Classes import Account
+
+def display():
+    obj = config.CURR_USER_ACC
+    obj.name
+    obj.balance
+    
+if __name__ == "__main__":
+    
